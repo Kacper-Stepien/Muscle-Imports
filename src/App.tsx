7bcff3a5e8f56da268c6cc37f456e797e5e1a8b1
Kacper Stępień <kacper2007x48@gmail.com>
2023-08-23 18:09:08 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div className="bg-primary">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
